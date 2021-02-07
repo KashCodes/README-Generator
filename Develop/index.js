676995094f97c3bbf94439c76903749c3ce66643
Kash <kashay@agyeman.me>
2021-02-07 11:09:41 -0700

    finished index.js

--- Develop/index.js
+++ Develop/index.js
@@ -71,7 +71,14 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  .prompt(questions)
+  .then(answers => {
+    console.log('Making README now.');
+    writeToFile('README.md', generateMarkdown({...answers}));
+  })
+}
 
 // Function call to initialize app
 init();
